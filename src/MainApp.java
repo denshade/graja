638258615c686d5c3fbb1e1123c61fd0eb8af8aa
Lieven <lieven.vaneeckhaute@gmail.com>
2023-01-15 22:20:14 +0100

    editor.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,10 +1,34 @@
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class MainApp extends JDialog
 {
     public static void main(String[] args)
     {
+        SwingUtilities.invokeLater(() -> new MainApp().setVisible(true));
+    }
+
+    public MainApp()
+    {
+        JPanel cp = new JPanel(new BorderLayout());
+
+        RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        textArea.setCodeFoldingEnabled(true);
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        cp.add(sp);
+
+        setContentPane(cp);
+        setTitle("Text Editor Demo");
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
 
     }
+
 
 }
